fb26bfea34979d26673919d44a8ed925f54953e8
Charlie Frey <charliesagefrey95@gmail.com>
2025-01-21 11:58:15 -0500

    escaping apos

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
           <div className="flex flex-col justify-center">
             <div className="m-2 px-6 py-4 cornerbox">
               <div className="text-3xl leading-loose">
-                I'm a <i>School Health and Wellness Education: K-12</i> major at
-                Slippery Rock University of Pennsylvania and a{" "}
+                I&apos;m a <i>School Health and Wellness Education: K-12</i>{" "}
+                major at Slippery Rock University of Pennsylvania and a{" "}
                 <strong>certified personal trainer</strong> based out of New
-                Castle, PA. I'll be graduating in Spring 2025 as a{" "}
+                Castle, PA. I&apos;ll be graduating in Spring 2025 as a{" "}
                 <i>Bachelor of Education</i>.
               </div>
             </div>
